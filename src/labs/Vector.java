620fa4299a609d036273b7816e1ac3c7d4ec50a2
artur <mauboroda.arturtu@gmail.com>
2025-02-18 14:44:10 +0200

    creat Vector and VectorApp.Java, relise task

---
+++ src/labs/Vector.java
@@ -0,0 +1,54 @@
+package labs;
+
+public class Vector {
+    private double x;
+    private double y;
+
+    public Vector() {
+        this.x = 0.0;
+        this.y = 0.0;
+    }
+
+    public Vector (double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public Vector (Vector v) {
+        this.x = v.x;
+        this.y = v.y;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public double getRadius(){
+        return Math.sqrt(x * x + y * y);
+    }
+
+    public double getAngle() {
+        return Math.atan2(y, x);
+    }
+
+    @Override
+    public String toString() {
+        return "Vector (" + x + ", " + y + ")";
+    }
+
+    @Override
+    protected void finalize() throws Throwable {
+        System.out.println("Vector" + this + " is being destroyed");
+    }
+}
---
+++ src/labs/VectorApp.java
@@ -0,0 +1,28 @@
+package labs;
+import java.util.Scanner;
+
+
+public class VectorApp {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        Vector v1 = new Vector();
+        Vector v2 = new Vector(3, 4);
+        Vector v3 = new Vector(v2);
+
+        System.out.println("Default vector: " + v1);
+        System.out.println("Vector with coordinates (3,4): " + v2);
+        System.out.println("Copied vector: " + v3);
+
+        System.out.print("Enter x coordinate: ");
+        double x = scanner.nextDouble();
+        System.out.print("Enter y coordinate: ");
+        double y = scanner.nextDouble();
+
+        Vector userVector = new Vector(x, y);
+        System.out.println("Your vector: " + userVector);
+        System.out.println("Polar coordinates: Radius = " + userVector.getRadius() + ", Angle = " + userVector.getAngle());
+
+        scanner.close();
+    }
+}
